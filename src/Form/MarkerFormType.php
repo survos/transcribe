efd34caed3865905fd139eba39b94e3c97171e3a
Tac Tacelosky <tacman@gmail.com>
2018-10-18 12:13:06 -0400

    add marker colors

--- src/Form/MarkerFormType.php
+++ src/Form/MarkerFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Marker;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,16 +13,16 @@ class MarkerFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $colors = ['red', 'cyan', 'green'];
         $builder
             ->add('title')
             ->add('note')
-            ->add('color')
-            ->add('idx')
-            ->add('firstWordIndex')
-            ->add('lastWordIndex')
-            ->add('media', null, [
-                'disabled' => true
+            ->add('color', ChoiceType::class, [
+                'choices' => array_combine($colors, $colors)
             ])
+            // ->add('idx')
+            ->add('firstWordIndex', HiddenType::class)
+            ->add('lastWordIndex', HiddenType::class)
         ;
     }
 
