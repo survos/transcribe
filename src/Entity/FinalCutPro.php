697f6ffb7dcf4e8fc2f6e88b5192395595832c98
tac <tacman@gmail.com>
2018-11-14 07:32:38 -0500

    add FinalCutPro entity

---
+++ src/Entity/FinalCutPro.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FinalCutProRepository")
+ */
+class FinalCutPro
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $version;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
+
+    public function setVersion(string $version): self
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+}
---
+++ src/Repository/FinalCutProRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\FinalCutPro;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method FinalCutPro|null find($id, $lockMode = null, $lockVersion = null)
+ * @method FinalCutPro|null findOneBy(array $criteria, array $orderBy = null)
+ * @method FinalCutPro[]    findAll()
+ * @method FinalCutPro[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FinalCutProRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, FinalCutPro::class);
+    }
+
+    // /**
+    //  * @return FinalCutPro[] Returns an array of FinalCutPro objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('f.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?FinalCutPro
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
